05dd677306d4887c49c46516836a9b6ac2c8c7a2
JasonCacante <jason.caccante@gmail.com>
2021-03-03 22:21:40 -0500

    Se hacen uns cambios para probar la extensión que colorea la identación

--- web_app.py
+++ web_app.py
@@ -1,3 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
+# *
+# *Comments parameter, variable app
+print("Lista de pares del 0 al 10")
+for i in range(11):
+    if i % 2 == 0:
+        print(i, end="-")
 
