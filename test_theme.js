92b4197957e0f973fa84f8212e1c976928f7b178
JasonCacante <jason.caccante@gmail.com>
2021-03-03 21:48:11 -0500

    Probando las extensón bracket pair colorizer

--- test_theme.js
+++ test_theme.js
@@ -1,2 +1,8 @@
 import param from param
 const test =param.param
+public updateDocument(document, TextDocument){
+    const documentDecoration = this.getDocumentDecorations(document);
+    if (documentDecoration){
+        documentDecoration.triggerUpdateDecoration();       
+    }
+}
